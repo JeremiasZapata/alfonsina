240d5ccc76227189b79cfd43f54c2b0209cd07ac
Jeremias <blackarrowsb@gmail.com>
2023-06-05 00:14:43 -0300

    Segundo intento

---
+++ src/pages/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Footer = () => {
+  return (
+    <div className="container-fluid">
+        <div className="raw ">
+            <div className="col">
+                <hr className="bg-dark"/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/pages/Start.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Start = () => {
+  return (
+    <div className="container-fluid">
+        <div className="row">
+            <div className="col bg-black vh-100" >
+                <h1 className='text-white'>Inicio</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Start
