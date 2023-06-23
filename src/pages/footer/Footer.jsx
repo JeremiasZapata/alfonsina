c055d03152be761e045081feec670a3860f7013a
Jeremias <blackarrowsb@gmail.com>
2023-06-23 09:45:59 -0300

    footer y nav sin dar mucho estilo

--- src/pages/footer/Footer.jsx
+++ src/pages/footer/Footer.jsx
@@ -4,7 +4,47 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <div className="FooterClass">
+    <div className="FooterClass black">
+      <footer className="page-footer black left-align">
+          <div className="container left-align p-0">
+            <div className="row">
+              
+              <div className="col l4 offset-l2 s6">
+                <h5 className="white-text">Links</h5>
+                <ul>
+                  <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
+              <div className="col l4 offset-l2 s6">
+                <h5 className="white-text">Links</h5>
+                <ul>
+                  <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
+              <div className="col l4 offset-l2 s3">
+                <h5 className="white-text">Links</h5>
+                <ul>
+                  <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="footer-copyright">
+            <div className="container">
+            © 2014 Copyright Text
+            <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </footer>
     </div>
   )
 }
--- src/pages/navbar/NavBar.jsx
+++ src/pages/navbar/NavBar.jsx
@@ -1,14 +1,62 @@
 import React from "react";
 import './NavBar.css'
 import { Link } from 'react-router-dom'
+import logo from "./images/Isotipo-3.png";
 
-export const NavBar = () =>{
-    return(
-        <div className="NavBar">
-            <Link to="/" className="LinkNavBar">Home</Link>
-            <Link to="/cursos" className="LinkNavBar">Cursos</Link>
-            <Link to="/nosotras" className="LinkNavBar">Sobre Nosotras</Link>
-            <Link to="/contacto" className="LinkNavBar">Contacto</Link>
+export const NavBar = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <nav class="navbar navbar-expand-lg bg-light">
+                        <div class="container-fluid">
+                            <a class="navbar-brand" href="#">Navbar</a>
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse" id="navbarNav">
+                                <ul class="navbar-nav">
+                                    <li class="nav-item">
+                                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Features</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Pricing</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link disabled">Disabled</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
         </div>
+
+        // <div>
+        //     <nav>
+        //         <div class="nav-wrapper">
+        //             <a href="#" class="brand-logo">Logo</a>
+        //             <ul id="nav-mobile" class="right hide-on-med-and-down">
+        //                 <li><a href="sass.html">Sass</a></li>
+        //                 <li><a href="badges.html">Components</a></li>
+        //                 <li><a href="collapsible.html">JavaScript</a></li>
+        //             </ul>
+        //         </div>
+        //     </nav>
+        // </div>
+
+
+        // <div className="NavBar">
+        //     <Link className="navbar-brand" to={"/"}> <img src={logo} alt="Logo Alfonsina Jostal" width={"90"}/> </Link>
+        //     <Link to="/" className="LinkNavBar">Home</Link>
+        //     <Link to="/cursos" className="LinkNavBar">Cursos</Link>
+        //     <Link to="/nosotras" className="LinkNavBar">Sobre Nosotras</Link>
+        //     <Link to="/contacto" className="LinkNavBar">Contacto</Link>
+        // </div>
+
     );
 }
