85c2c299e182a777e2a8f2fe3a34f64ce00666b1
Gialdini Javier <jngialdini19@gmail.com>
2023-06-24 00:19:52 -0300

    Detalle de nivel y de cursada para el right side de la card de cursos

--- src/pages/cursos/Cursos.jsx
+++ src/pages/cursos/Cursos.jsx
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { HeroCursos } from "./heroCursos/HeroCursos"
 import { CardCursos } from "./cardCursos/CardCursos"
 import "./Cursos.css";
 
+import MOCK_DATA from '../../Data/mockCursos.json'
+
+export const pedirDatos = (bool) => {
+    return new Promise((resolve, reject) => {
+        // cuerpo de la promesa
+        setTimeout(()=>{
+            resolve(MOCK_DATA)
+        }, 500)
+    })
+}
+
 export const Cursos = () => {
+    const [datosCursos, setDatosCursos] = useState([]);
+
+    useEffect(() => {
+        const obtenerDatos = async () => {
+        try {
+            const datos = await pedirDatos();
+            setDatosCursos(datos);
+        } catch (error) {
+            console.error("Error al obtener los datos de los cursos:", error);
+        }
+        };
+
+        obtenerDatos();
+    }, []);
+
+    console.log(datosCursos)
     return(
         <div className="containerCursos">
             <HeroCursos/>
             <div className="containerDivCursos">
-                <CardCursos/>
+                {
+                    datosCursos.map((curso) => (
+                        <CardCursos key={curso.nombre}/>
+                    ))
+                }
             </div>
         </div>
     );
--- src/pages/cursos/cardCursos/cardRightSideComponents/CardRightSide.jsx
+++ src/pages/cursos/cardCursos/cardRightSideComponents/CardRightSide.jsx
@@ -1,10 +1,46 @@
 import React from "react";
 import "./CardRightSide.css"
+import icoLocation from "../../../../Icons/location.svg"
+import icoCalendar from "../../../../Icons/calendar.svg"
+import icoClock from "../../../../Icons/clock.svg"
+
 
 export const CardRightSide = () => {
     return(
         <div className="CardRightSide">
-
+            <div className="NivelCursoText">
+                Inicial - No requiere conocimientos previos
+            </div>
+            <div className="DetalleCursadaContainer">
+                <div className="IcoLocation">
+                    <img alt="icoLocation" src={icoLocation} />
+                </div>
+                <div class="DetalleModalidadContainer">
+                    <div className="DetalleModalidadTitulo">
+                        Modalidad:
+                    </div>
+                    <div className="DetalleModalidad">Presencial</div>
+                </div>
+                <div className="IcoCalendar">
+                    <img alt="icoCalendar" src={icoCalendar} />
+                </div>
+                <div class="DetalleDuracionContainer">
+                    <div className="DetalleDuracionTitulo">
+                        Duración:
+                    </div>
+                    <div className="DetalleDuracion">5 meses</div>
+                </div>
+                <div className="IcoClock">
+                    <img alt="icoClock" src={icoClock} />
+                </div>
+                <div class="DetalleFrecuenciaContainer">
+                    <div className="DetalleFrecuenciaTitulo">
+                        Frecuencia:
+                    </div>
+                    <div className="DetalleFrecuencia">1 clase semanal de 3hs</div>
+                </div>
+            </div>
+            <hr className="LineaDivisoria"/>
         </div>
     );
 }
