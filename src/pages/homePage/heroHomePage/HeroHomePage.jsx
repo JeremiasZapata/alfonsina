359c786a61138e8fbd068c9870d04eb0225ef058
Gialdini Javier <jngialdini19@gmail.com>
2023-06-25 19:22:32 -0300

    Reduzco 80p hero Home

--- src/pages/homePage/heroHomePage/HeroHomePage.jsx
+++ src/pages/homePage/heroHomePage/HeroHomePage.jsx
@@ -4,7 +4,7 @@ import './HeroHomePage.css'
 export const HeroHomePage = () =>{
     return(
         <div className="HomePage">
-            <div className="HomeTitleClass">Alfonsina Jostal Studio</div>
+            <div className="HomeTitleClass">Lorem ipsum dolor sit amet consectetur.</div>
         </div>
     );
 }
