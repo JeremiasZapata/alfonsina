e4b3bcdc59e5ff8d7be9e65c904d467539ace587
Jeremias <blackarrowsb@gmail.com>
2023-07-03 13:02:10 -0300

    ojala no se haya perdido

--- src/pages/navbar/NavBar.jsx
+++ src/pages/navbar/NavBar.jsx
@@ -28,25 +28,21 @@ import './NavBar.css'
 import { Link } from 'react-router-dom'
 import { Navbar, Nav } from 'react-bootstrap';
 
-export const NavBar = () => {
-  return (
-    <div>
-      <div className="NavBarBrandWrapper">
-        <Navbar.Brand className="NavBarBrand"></Navbar.Brand>
-      </div>
-      <Navbar expand="lg" className="NavBar">
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Link to="/" className="LinkNavBar">Home</Link>
-            <Link to="/cursos" className="LinkNavBar">Cursos</Link>
-            <Link to="/nosotras" className="LinkNavBar">Sobre Nosotras</Link>
-            <Link to="/contacto" className="LinkNavBar">Contacto</Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
-  );
+export const NavBar = () =>{
+    return(
+        <Navbar expand="lg  NavBar">
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Brand className="NavBarBrand">Mi Sitio</Navbar.Brand>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                    <Link to="/" className="LinkNavBar">Home</Link>
+                    <Link to="/cursos" className="LinkNavBar">Cursos</Link>
+                    <Link to="/nosotras" className="LinkNavBar">Sobre Nosotras</Link>
+                    <Link to="/contacto" className="LinkNavBar">Contacto</Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
 }
 
 
