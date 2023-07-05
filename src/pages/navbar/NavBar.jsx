fa3aa5cbf2901f44ccc08dbbf5ae9bc3473f9369
Gialdini Javier <jngialdini19@gmail.com>
2023-07-04 21:52:44 -0300

    Responsive NavBar

--- src/pages/navbar/NavBar.jsx
+++ src/pages/navbar/NavBar.jsx
@@ -1,41 +1,26 @@
-// import React from "react";
-// import './NavBar.css'
-// import { Link } from 'react-router-dom'
-// import { Navbar, Nav } from 'react-bootstrap'; // Importa los componentes necesarios de react-bootstrap
-
-// export const NavBar = () =>{
-//     return(
-//         <div>
-//             <Navbar expand="lg  NavBar">
-//                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//                 <Navbar.Brand className="NavBarBrand"></Navbar.Brand>
-//                 <Navbar.Collapse id="basic-navbar-nav">
-//                     <Nav className="ml-auto">
-//                         <Link to="/" className="LinkNavBar">Home</Link>
-//                         <Link to="/cursos" className="LinkNavBar">Cursos</Link>
-//                         <Link to="/nosotras" className="LinkNavBar">Sobre Nosotras</Link>
-//                         <Link to="/contacto" className="LinkNavBar">Contacto</Link>
-//                     </Nav>
-//                 </Navbar.Collapse>
-//             </Navbar>
-//         </div>
-//     );
-// }
-
-
 import React from "react";
 import './NavBar.css'
 import { Link } from 'react-router-dom'
 import { Navbar, Nav } from 'react-bootstrap';
+import Logo from '../assets/Logotipo-1.png'
 
 
 export const NavBar = () => {
   return (
     <div>
       <div className="NavBarBrandWrapper">
-        <Navbar.Brand className="NavBarBrand"></Navbar.Brand>
+        <div className="NavBarBrand">
+          <img className="Logo" alt="Logo" src={Logo} />
+        </div>
       </div>
-      <Navbar expand="lg" className="NavBar">
+      <div className="NavBar">
+        <Link to="/" className="LinkNavBar">Home</Link>
+        <Link to="/cursos" className="LinkNavBar">Cursos</Link>
+        <Link to="/nosotras" className="LinkNavBar">Sobre Nosotras</Link>
+        <Link to="/contacto" className="LinkNavBar">Contacto</Link>
+      </div>
+
+      {/* <Navbar expand="lg" className="NavBar">
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
@@ -45,7 +30,7 @@ export const NavBar = () => {
             <Link to="/contacto" className="LinkNavBar">Contacto</Link>
           </Nav>
         </Navbar.Collapse>
-      </Navbar>
+      </Navbar> */}
     </div>
   );
 }
