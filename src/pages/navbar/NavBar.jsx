d04672e9a9c8a75d460ccd90ca587aa8587c831f
Gialdini Javier <jngialdini19@gmail.com>
2023-07-08 17:33:30 -0300

    Nuevo navBar

--- src/pages/navbar/NavBar.jsx
+++ src/pages/navbar/NavBar.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import './NavBar.css'
-import { Link } from 'react-router-dom'
-import { Navbar, Nav } from 'react-bootstrap';
 import Logo from '../assets/Logotipo-1.png'
+import { CompactNavBar } from './CompactNavBar'
+
 
 
 export const NavBar = () => {
@@ -13,24 +13,7 @@ export const NavBar = () => {
           <img className="Logo" alt="Logo" src={Logo} />
         </div>
       </div>
-      <div className="NavBar">
-        <Link to="/" className="LinkNavBar">Home</Link>
-        <Link to="/cursos" className="LinkNavBar">Cursos</Link>
-        <Link to="/nosotras" className="LinkNavBar">Sobre Nosotras</Link>
-        <Link to="/contacto" className="LinkNavBar">Contacto</Link>
-      </div>
-
-      {/* <Navbar expand="lg" className="NavBar">
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Link to="/" className="LinkNavBar">Home</Link>
-            <Link to="/cursos" className="LinkNavBar">Cursos</Link>
-            <Link to="/nosotras" className="LinkNavBar">Sobre Nosotras</Link>
-            <Link to="/contacto" className="LinkNavBar">Contacto</Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar> */}
+      <CompactNavBar/>
     </div>
   );
 }
