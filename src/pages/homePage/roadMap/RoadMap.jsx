44d71a07f16b83baf401a88833ff404f86110383
Gialdini Javier <jngialdini19@gmail.com>
2023-07-12 22:13:14 -0300

    roadMap

--- src/pages/homePage/roadMap/RoadMap.jsx
+++ src/pages/homePage/roadMap/RoadMap.jsx
@@ -2,12 +2,31 @@ import React, { useEffect, useState } from "react";
 import "./RoadMap.css"
 // import { Scrollama, Step } from 'react-scrollama';
 
+// MapaConceptualTittleContainer
+// MapaConceptualConditionalContainer
+// MapaConceptualCloudContainer
+// MapaConceptualResultContainer
+
 export const RoadMap = () =>{
     return(
         <div className="RoadMap">
-            <div className="RoadMapTittleContainer">
-                <div className="RoadMapTittleText">
-                ¡Elegí el curso ideal para vos!
+            <div className="RoadMapGrid">
+                <div className="RoadMapTittleContainer">
+                    <div className="RoadMapTittleText">
+                    ¡Elegí el curso ideal para vos!
+                    </div>
+                </div>
+                <div className="MapaConceptualTittleContainer">
+
+                </div>
+                <div className="MapaConceptualConditionalContainer">
+
+                </div>
+                <div className="MapaConceptualCloudContainer">
+
+                </div>
+                <div className="MapaConceptualResultContainer">
+
                 </div>
             </div>
         </div>
