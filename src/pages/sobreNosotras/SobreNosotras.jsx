53731221bb08c8e041054cdf1421007bf5b7d324
Jeremias <blackarrowsb@gmail.com>
2023-08-01 09:09:08 -0300

    creacion de bienvenida

--- src/pages/sobreNosotras/SobreNosotras.jsx
+++ src/pages/sobreNosotras/SobreNosotras.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { HeroSobreNosotras } from "./heroSobreNosotras/HeroSobreNosotras"
+import Bienvenida from "./bienvenida/Bienvenida";
 
 export const SobreNosotras = () => {
     return(
         <div className="containerSobreNosotras">
             <HeroSobreNosotras/>
+            <Bienvenida/>
         </div>
     );
 }
