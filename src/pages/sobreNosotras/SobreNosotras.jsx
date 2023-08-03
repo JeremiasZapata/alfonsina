37531537dc00b27e790c73c9efa124fc45f4171a
Jeremias <blackarrowsb@gmail.com>
2023-08-03 07:23:55 -0300

    bienvenidos sobre nos

--- src/pages/sobreNosotras/SobreNosotras.jsx
+++ src/pages/sobreNosotras/SobreNosotras.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { HeroSobreNosotras } from "./heroSobreNosotras/HeroSobreNosotras"
 import Bienvenida from "./bienvenida/Bienvenida";
+import '../sobreNosotras/SobreNosotras.css'
 
 export const SobreNosotras = () => {
     return(
--- src/pages/sobreNosotras/bienvenida/Bienvenida.jsx
+++ src/pages/sobreNosotras/bienvenida/Bienvenida.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import clase1 from "../../assets/imagenesSobreNosotras/clases1.png"
+import clase2 from "../../assets/imagenesSobreNosotras/clases2.png"
+import clase3 from "../../assets/imagenesSobreNosotras/clases3.png"
+import './Bienvenida.css'
 
-const Bienvenida = () => {
-  return (
-    <div>Bienvenida</div>
-  )
-}
+const TextoConImagenes = () => {
+    return (
+        <div className="container bienvenida">
+            <h2 className='titulo'>¡Te damos la bienvenida a nuestra academia!</h2>
+            <p>Somos un equipo capacitado y preparado para darte una educación de calidad. Con nuestra experiencia y dedicación, buscamos que nuestros alumnos aprendan y mejoren sus técnicas para que sean excelentes profesionales en su área.</p>
+            <p>Para ello, contamos con programas actualizados, cursos presenciales en instalaciones cómodas y educadores comprometidos. También es la razón por la que no prestamos el kit de uñas y alentamos a cada estudiante obtener uno propio para que practique fuera de clase.</p>
+            <div className="imagenes-container">
+                <img
+                    className="imagen1"
+                    src={clase1}
+                    alt="Descripción de la imagen 1"
+                />
+                <img
+                    className="imagen2"
+                    src={clase2}
+                    alt="Descripción de la imagen 2"
+                />
+                <img
+                    className="imagen3"
+                    src={clase3}
+                    alt="Descripción de la imagen 3"
+                />
+            </div>
+        </div>
+    );
+};
 
-export default Bienvenida
+export default TextoConImagenes;
